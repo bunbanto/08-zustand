9aa35d9744a5b99996b9a2e2c4cbe4a62f0f555e
yurii_fishbakh <bunbanto@gmail.com>
2025-07-03 16:43:02 +0200

    meta done

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -10,6 +10,36 @@ type Props = {
   params: Promise<{ id: string }>;
 };
 
+export async function generateMetadata({ params }: Props) {
+  const { id } = await params;
+  const note = await getNoteById(Number(id));
+  return {
+    title: `Note: ${note.title}`,
+    description: note.content.slice(0, 30),
+    openGraph: {
+      title: `Note: ${note.title}`,
+      description: note.content.slice(0, 30),
+      url: `https://08-zustand-snowy.vercel.app/notes/${id}`,
+      siteName: 'NoteHub',
+      images: [
+        {
+          url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+          width: 1200,
+          height: 630,
+          alt: note.title,
+        },
+      ],
+      type: 'article',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: `${note.title}`,
+      description: note.content.slice(0, 30),
+      images: ['https://ac.goit.global/fullstack/react/notehub-og-meta.jpg'],
+    },
+  };
+}
+
 const NoteDetails = async ({ params }: Props) => {
   const { id } = await params;
   const queryClient = new QueryClient();
--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -5,6 +5,37 @@ interface Props {
   params: Promise<{ slug: string[] }>;
 }
 
+export async function generateMetadata({ params }: Props) {
+  const { slug } = await params;
+  const tag = slug[0] === 'all' ? undefined : slug[0];
+  const pageTitle = tag ? `Notes - ${tag}` : 'Notes - All notes';
+  return {
+    title: pageTitle,
+    description: `Notes filtered by ${tag || 'All notes'}`,
+    openGraph: {
+      title: pageTitle,
+      description: `Notes filtered by ${tag || 'All notes'}`,
+      url: `https://08-zustand-snowy.vercel.app/notes/filter/${slug.join('/')}`,
+      siteName: 'NoteHub',
+      images: [
+        {
+          url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+          width: 1200,
+          height: 630,
+          alt: pageTitle,
+        },
+      ],
+      type: 'website',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: pageTitle,
+      description: `Notes filtered by ${tag || 'All notes'}`,
+      images: ['https://ac.goit.global/fullstack/react/notehub-og-meta.jpg'],
+    },
+  };
+}
+
 const Page = async ({ params }: Props) => {
   const { slug } = await params;
   const tag = slug[0] === 'all' ? undefined : slug[0];
